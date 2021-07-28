79245cd70cf8e2483c8b7210830b9eb49025703b
ziedjalajel <zied.jalajel1993@gmail.com>
2021-07-28 22:51:27 +0300

    added auth & some styling

--- src/store/reducer/rootReducer.js
+++ src/store/reducer/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import chatReducer from "./chatReducer";
 import messageReducer from "./messageReducer";
 import profileReducer from "./profileReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
   chats: chatReducer,
   messages: messageReducer,
   profiles: profileReducer,
+  user: authReducer,
 });
 export default rootReducer;
